c14c0ae36c8e73e2ff671b0e7fa3ee68aa3acaa8
fmamah777 <fm.mamah@gmail.com>
2023-03-09 15:54:42 -0600

    -i flag removed

--- module5_task1/ensure-server-setup.sh
+++ module5_task1/ensure-server-setup.sh
@@ -13,7 +13,7 @@ aws ec2 wait instance-running --instance-ids $ec2InstanceId
 # Start Docker: sudo systemctl start docker
 # Make Docker start on boot of the system: sudo systemctl enable docker
 # Add the ubuntu user to the docker group - this will let us use ssh to communicate with docker from a client: sudo usermod -aG docker ubuntu
-ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem -o ConnectTimeout=180 ubuntu@"$1" "
+ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=180 ubuntu@"$1" "
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install docker.io unzip -y
@@ -22,5 +22,5 @@ ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem -o ConnectTime
 	sudo usermod -aG docker ubuntu
 "
 
-# Save the IP address for other files to refer to
+# Save the IP address for other files to refer too
 echo "$1" > new_ec2_instance_public_ip.txt
