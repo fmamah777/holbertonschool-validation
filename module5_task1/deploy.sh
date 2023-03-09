bb60f9ab9d3c449076b8feb9239795420f317bf8
fmamah777 <fm.mamah@gmail.com>
2023-03-09 15:46:09 -0600

    deploy correction

--- module5_task1/deploy.sh
+++ module5_task1/deploy.sh
@@ -4,9 +4,9 @@
 new_ec2_instance_public_ip=$(cat awesome_image_tag_name.txt)
 dockerImageTag=$(cat awesome_image_tag_name.txt)
 
-scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
+scp -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
 
-ssh -o StrictHostKeyChecking=accept-new ubuntu@"$new_ec2_instance_public_ip" "
+ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem ubuntu@"$new_ec2_instance_public_ip" "
 	unzip ./awesome.tar.zip
 	docker load --input awesome.tar
 	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$dockerImageTag
--- module5_task1/ensure-server-setup.sh
+++ module5_task1/ensure-server-setup.sh
@@ -13,7 +13,7 @@ aws ec2 wait instance-running --instance-ids $ec2InstanceId
 # Start Docker: sudo systemctl start docker
 # Make Docker start on boot of the system: sudo systemctl enable docker
 # Add the ubuntu user to the docker group - this will let us use ssh to communicate with docker from a client: sudo usermod -aG docker ubuntu
-ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=180 ubuntu@"$1" "
+ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem -o ConnectTimeout=180 ubuntu@"$1" "
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install docker.io unzip -y
