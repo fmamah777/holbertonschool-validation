d8cf1ee2e227a9452fd76af6903ca2b31846bdec
fmamah777 <fm.mamah@gmail.com>
2023-03-15 04:07:54 -0500

    mod5/t2

--- module5_task2/deploy.sh
+++ module5_task2/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Read the IP address from the file
-new_ec2_instance_public_ip=$(cat awesome_image_tag_name.txt)
+new_ec2_instance_public_ip=$(cat new_ec2_instance_public_ip.txt)
+
+# Read the name of the tag to use with the docker image
 dockerImageTag=$(cat awesome_image_tag_name.txt)
 
-scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
+docker context create production --docker "host=ssh://ubuntu@$new_ec2_instance_public_ip"
+docker context use production
 
-ssh -o StrictHostKeyChecking=accept-new ubuntu@"$new_ec2_instance_public_ip" "
-	unzip ./awesome.tar.zip
-	docker load --input awesome.tar
-	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$dockerImageTag
-"
+docker pull tsuroo/awesome-web:"$dockerImageTag"
+docker compose up --detach awesome-web
