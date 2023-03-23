171b93744277e9c0a8e57a576309c1d7ba6eb6b8
fmamah777 <fm.mamah@gmail.com>
2023-03-23 11:22:08 -0500

    mod 7 jenkins serv

--- module7_task2/ensure-server-created.sh
+++ module7_task2/ensure-server-created.sh
@@ -9,8 +9,8 @@ fi
 aws ec2 run-instances \
 	--image-id ami-0568936c8d2b91c4e \
 	--instance-type t2.micro \
-	--subnet-id subnet-0584ee42a38f71993 \
-	--security-group-ids sg-0983c9a30720ce76a \
+	--subnet-id subnet-02e94098977dfa45d \
+	--security-group-ids sg-09f59566ca50f9b1e \
 	--key-name awesome-key \
 	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" \
 	--query 'Instances[*].InstanceId' \
